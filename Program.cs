c2e0dc6139ccf31c695280ff4cf340af76d3a79b
HostilePuma <93294561+HostilePuma@users.noreply.github.com>
2021-10-27 18:36:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! ALEX JONES FANBOI HERE");
+Console.WriteLine("This is a simple placeholder for the future video game: Alex Jones vs The Globalists");
+Console.WriteLine("Until release, please keep in mind THEY'RE TURNING THE FROGS GAY\nand 1776 WILL COME AGAIN\nPress \"Enter\" to exit.");
+Console.Read();
+
